94a4922a4930c31f58d5505ec6e242c2865f12d4
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2025-08-29 13:59:14 +0530

    Create DB

---
+++ scripts/init_db.sql
@@ -0,0 +1,21 @@
+USE master;
+GO
+
+-- Drop and recreate the 'CityTrafficDB' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'CityTrafficDB')
+BEGIN
+    ALTER DATABASE CityTrafficDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE CityTrafficDB;
+END;
+GO
+
+-- Create the 'CityTrafficDB' database
+CREATE DATABASE CityTrafficDB;
+GO
+
+USE CityTrafficDB;
+
+
+CREATE TABLE TrafficAccidents (
+
+);
