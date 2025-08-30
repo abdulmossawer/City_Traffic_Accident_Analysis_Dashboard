ae68bebd34237a6136a07b30eb89fad689c55d8f
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2025-08-30 17:03:29 +0530

    Update insights.sql

--- scripts/insights.sql
+++ scripts/insights.sql
@@ -2,24 +2,25 @@ SELECT * FROM RoadAccidents
 
 
 -- Accidents per Year
+CREATE VIEW TotalAccidentByYear AS
 SELECT 
 	YEAR(AccidentDate) AS AccYear,
 	COUNT(*) AS TotalAccidentByYear
 FROM RoadAccidents
 GROUP BY YEAR(AccidentDate)
-ORDER BY TotalAccidentByYear DESC;
 
 
 -- Accidents per Month
+CREATE VIEW TotalAccidentByMonth AS
 SELECT
 	DATENAME(MONTH, AccidentDate) AS AccMonth,
 	COUNT(*) AS TotalAccidentByMonth
 FROM RoadAccidents
 GROUP BY DATENAME(MONTH, AccidentDate)
-ORDER BY TotalAccidentByMonth DESC;
 
 
 -- Accidents by Day of Week
+CREATE VIEW TotalAccidentByDay AS
 SELECT
 	Day_of_Week,
 	COUNT(*) AS TotalAccidentByDay
@@ -29,6 +30,7 @@ ORDER BY TotalAccidentByDay DESC;
 
 
 -- Accident Count by Severity
+CREATE VIEW TotalSeverity AS
 SELECT
 	Accident_Severity,
 	COUNT(*) AS TotalSeverity
@@ -38,22 +40,25 @@ ORDER BY TotalSeverity DESC;
 
 
 -- Average Casualties per Accident
+CREATE VIEW AvgCasualties AS
 SELECT
-	AVG(Number_of_Casualties) AS TotalCasualties
+	AVG(TRY_CAST(Number_of_Casualties AS FLOAT)) AS TotalCasualties
 FROM RoadAccidents;
 
 
 -- Fatal Accidents by Month
+CREATE VIEW FatalAccidentsByMonth AS
 SELECT
-	DATENAME(MONTH, AccidentDate) AS AccMonth,
-	COUNT(*) AS TotalAccidentByMonth
+	DATENAME(MONTH, AccidentDate) AS AccidentMonth,
+	COUNT(*) AS FatalAccidentByMonth
 FROM RoadAccidents
 WHERE Accident_Severity = 'Fatal'
 GROUP BY DATENAME(MONTH, AccidentDate)
-ORDER BY TotalAccidentByMonth DESC;
+ORDER BY FatalAccidentByMonth DESC;
 
 
 -- Accidents by Road Surface
+CREATE VIEW AccidentsByRoadSurface AS
 SELECT
 CASE
 	WHEN TRIM(Road_Surface_Conditions) IS NULL THEN 'Unknown'
@@ -66,6 +71,7 @@ ORDER BY SurfaceConditions DESC;
 
 
 -- Weather Impact on Fatal Accidents
+CREATE VIEW WeatherImpact AS
 SELECT
 CASE
 	WHEN TRIM(Weather_Conditions) IS NULL THEN 'Unknown'
@@ -79,6 +85,7 @@ ORDER BY FatalAcc DESC;
 
 
 -- Accidents by Lighting Condition
+CREATE VIEW LightingCondition AS
 SELECT
 	Light_Conditions,
 	COUNT(*) AS TotalAcc
@@ -88,6 +95,7 @@ ORDER BY TotalAcc DESC;
 
 
 -- Accidents by Vehicle Type
+CREATE VIEW VehicleType AS
 SELECT
 	Vehicle_Type,
 	COUNT(*) AS TotalAcc
@@ -97,6 +105,7 @@ ORDER BY TotalAcc DESC;
 
 
 -- Severity by Vehicle Type
+CREATE VIEW SeverityByVehicleType AS
 SELECT
 	Accident_Severity,
 	Vehicle_Type,
@@ -107,6 +116,7 @@ ORDER BY Total DESC
 
 
 -- Average Casualties by Vehicle Type
+CREATE VIEW AverageCasualtiesVehicle AS 
 SELECT
 	Vehicle_Type,
 	AVG(TRY_CAST(Number_of_Casualties AS FLOAT)) AS AvgCasualties
@@ -116,6 +126,7 @@ ORDER BY AvgCasualties DESC;
 
 
 -- Accidents by Region
+CREATE VIEW AccidentsByRegion AS
 SELECT
 	Urban_or_Rural_Area,
 	COUNT(*) AS Total
@@ -125,15 +136,17 @@ ORDER BY Total DESC;
 
 
 -- Top Locations with Most Accidents
+CREATE VIEW TopLocationsAccidents AS 
 SELECT TOP 10
 	Local_Authority_District,
-	COUNT(*) Total
+	COUNT(*) TotalAccidents
 FROM RoadAccidents
 GROUP BY Local_Authority_District
-ORDER BY Total DESC;
+ORDER BY TotalAccidents DESC;
 
 
 -- Accidents by Speed Limit
+CREATE VIEW AccidentsBySpeedLimit AS
 SELECT 
 	Speed_limit,
 	COUNT(*) TotalAcc
@@ -141,12 +154,14 @@ FROM RoadAccidents
 GROUP BY Speed_limit
 ORDER BY TotalAcc DESC;
 
+CREATE VIEW AvgSpeed AS
 SELECT 
 	AVG(Speed_limit) AvgSpeed
 FROM RoadAccidents;
 
 
 -- Accidents on Road Type
+CREATE VIEW RoadType AS
 SELECT
 CASE
 	WHEN TRIM(Road_Type) IS NULL THEN 'Unknown'
@@ -159,6 +174,7 @@ ORDER BY TotalAcc DESC;
 
 
 -- Peak Accident Hours
+CREATE VIEW PeakAccidentHours AS
 SELECT 
     DATEPART(HOUR, Time) AS HourOfDay,
     COUNT(*) AS TotalAccidents
@@ -169,6 +185,7 @@ ORDER BY TotalAccidents DESC;
 
 
 -- Day vs Night Accidents
+CREATE VIEW DayNightAccidents AS
 SELECT
 CASE
 	WHEN DATEPART(HOUR, Time) BETWEEN 6 AND 18 THEN 'Day'
@@ -182,6 +199,7 @@ ORDER BY TotalAccidents DESC;
 
 
 -- Accidents Hotspots by Lat/Long
+CREATE VIEW AccidentsLatLong AS
 SELECT 
 	Latitude,
 	Longitude,
@@ -192,6 +210,7 @@ ORDER BY TotalAccidents DESC;
 
 
 -- Fatality Rate per Year
+CREATE VIEW FatalityRateYear AS
 SELECT 
     YEAR(AccidentDate) AS AccYear,
     SUM(CASE WHEN Accident_Severity = 'Fatal' THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS FatalityRate
